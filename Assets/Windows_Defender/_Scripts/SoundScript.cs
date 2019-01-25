48962716190691f2c5e7b03dbb44a342dd73849b
UltraBose13 <47039692+UltraBose13@users.noreply.github.com>
2019-01-26 00:53:16 +0100

    Fancy Stuff

--- Assets/Windows_Defender/_Scripts/SoundScript.cs
+++ Assets/Windows_Defender/_Scripts/SoundScript.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class SoundScript : MonoBehaviour
 {
-    
-    int randomSound;
+    public GameObject WindowsHome;
+    public AudioClip[] DeathSounds;
+    public AudioClip[] StartUpSounds;
+       int randomSound;
+    bool playdeath;
     // Start is called before the first frame update
-  void PlayDeathSound()
+    private void Start()
     {
-        
+        PlayStartSound();
+        playdeath = true;
+    }
+    private void Update()
+    {
+        if (WindowsHome == null&&playdeath == true)
+        {
+            PlayDeathSound();
+            playdeath = false;
+        }
+    }
+
+    public void PlayDeathSound()
+    {
+        randomSound = Random.Range(0,DeathSounds.Length);
+        GetComponent<AudioSource>().PlayOneShot(DeathSounds[randomSound]);
+    }
+    public void PlayStartSound()
+    {
+        randomSound = Random.Range(0, StartUpSounds.Length);
+        GetComponent<AudioSource>().PlayOneShot(StartUpSounds[randomSound]);
     }
 }
