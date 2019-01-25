79c72ce080be14306f98b8d3a9063ed03ec491da
WaifuGuardian <sebastianzychlewicz14@gmail.com>
2019-01-26 00:52:26 +0100

    The Windows button along with its script
    Yeah^^^^

---
+++ Assets/Windows_Defender/_Scripts/WindowsButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowsButton : MonoBehaviour
+{
+    public int health = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision Col)
+    {
+        if (Col.gameObject.tag == "Enemy")
+        {
+            health--;
+            Destroy(Col.gameObject);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
