5b4b15bc007542967ece9a68568df1cbbe73cf3c
TasteMyBensson <47039731+TasteMyBensson@users.noreply.github.com>
2019-01-25 22:18:24 +0100

    Base enemy
    Base enemy klassen, och textures folder

---
+++ Assets/Windows_Defender/_Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Enemy : MonoBehaviour
+{
+    protected Rigidbody2D rigidbody;
+
+    protected float movementSpeed;
+    protected float attackPower;
+
+    protected int direction;
+
+    public void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void Update()
+    {
+        rigidbody.velocity = (Vector2.right * direction * movementSpeed) + (Vector2.up * rigidbody.velocity.y);
+    }
+
+    public void SetDirection(int d)
+    {
+        direction = d;
+    }
+}
---
+++ Assets/Windows_Defender/_Scripts/StandardEnemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Enemy))]
+public class StandardEnemy : Enemy
+{
+    void Start()
+    {
+        base.Start();
+
+        movementSpeed = 5;
+        direction = 1;
+    }
+}
