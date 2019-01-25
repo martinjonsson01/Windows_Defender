d72e9cb367a4a44faf96896e4042a885ec4e9b56
UltraBose13 <UltraBose13@users.noreply.github.com>
2019-01-25 21:41:34 +0100

    High Quality Sound Files
    HandCrafted by yours truly

---
+++ Assets/Windows_Defender/_Scripts/SoundScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour
+{
+    
+    int randomSound;
+    // Start is called before the first frame update
+  void PlayDeathSound()
+    {
+        
+    }
+}
