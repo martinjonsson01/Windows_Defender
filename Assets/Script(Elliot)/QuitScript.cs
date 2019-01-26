7daefa10ab7a164c88fb067fb681816c894d3457
WhoreOfTheAllMightBose <47039906+WhoreOfTheAllMightBose@users.noreply.github.com>
2019-01-26 21:04:03 +0100

    lyssnade på martin
    Bytte min kod till  void OnMouseDown()

--- Assets/Script(Elliot)/QuitScript.cs
+++ Assets/Script(Elliot)/QuitScript.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StartScript : MonoBehaviour
+public class QuitScript : MonoBehaviour
 {
     void OnMouseDown()
     {
-        print("nibber");
+        print("nej");
     }
 }
