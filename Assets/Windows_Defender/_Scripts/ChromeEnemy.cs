a30c4e39604174eaade605414be6c7e9a9d8ea3c
TasteMyBensson <47039731+TasteMyBensson@users.noreply.github.com>
2019-01-26 18:09:49 +0100

    Fiender byter håll som de ska nu

---
+++ Assets/Windows_Defender/_Scripts/ChromeEnemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Enemy))]
+public class ChromeEnemy : Enemy
+{
+    float rotationSpeed;
+    float rotation;
+
+    new void Start()
+    {
+        base.Start();
+
+        movementSpeed = 8;
+        rotationSpeed = (transform.localScale.x * 2) * Mathf.PI * movementSpeed * movementSpeed;
+
+        canFlipSprite = false;
+        SetDirection(1);
+    }
+
+    new void Update()
+    {
+        base.Update();
+
+        rotation += rotationSpeed * -direction * Time.deltaTime;
+        transform.eulerAngles = new Vector3(0, 0, rotation);
+    }
+}
---
+++ Assets/Windows_Defender/_Scripts/GlitchMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlitchMovement : MonoBehaviour
+{
+    Enemy enemy;
+
+    Rigidbody2D rigidbody;
+
+    float glitchTimer;
+    float currentRange;
+
+    void Start()
+    {
+        enemy = GetComponent<Enemy>();
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        NewTime();
+    }
+
+    void Update()
+    {
+        glitchTimer -= Time.deltaTime;
+
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, Vector2.right * enemy.GetDirection(), currentRange);
+
+        if(glitchTimer <= 0 && !RaycastContainsTag(hits, WindowTags.ALLTAGS))
+        {
+            rigidbody.MovePosition(
+                transform.position + Vector3.right * enemy.GetDirection() * currentRange
+                );
+
+            NewTime();
+        }
+    }
+
+    bool RaycastContainsTag(RaycastHit2D[] hits, string[] tags)
+    {
+        for (int i = 0; i < hits.Length; i++)
+            for(int j = 0; j < tags.Length; j++)
+                if (hits[i].collider.gameObject.tag == tags[j])
+                    return true;
+
+        return false;
+    }
+
+    void NewTime()
+    {
+        glitchTimer = Random.Range(2, 5);
+        currentRange = Random.Range(2, 4);
+    }
+}
