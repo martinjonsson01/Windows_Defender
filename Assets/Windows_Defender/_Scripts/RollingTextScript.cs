0b02340753729cfd090d1355138e88189cceb928
WetGargoyle <levultor@gmail.com>
2019-01-26 11:03:55 +0100

    Trying to commit again since my marriage is going downhill and my wife took everythign I did in unity
    ill never fall in love again

---
+++ Assets/Windows_Defender/_Scripts/RollingTextScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RollingTextScript : MonoBehaviour
+{
+    public float delay = 0.1f;
+    public string fullText;
+    private string currentText = "";
+    TextMeshPro TextPro;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextPro = GetComponent<TextMeshPro>();
+        StartCoroutine(ShowText());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator ShowText()
+    {
+        for (int i = 0; i <= fullText.Length; i++)
+        {
+            currentText = fullText.Substring(0, i);
+            TextPro.text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
